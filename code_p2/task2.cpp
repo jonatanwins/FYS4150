bd3e3a30c86ef6d5c9e40bd9424f50e70610304a
Jonatan <jonatankwins@gmail.com>
2024-09-13 16:25:27 +0200

    task4 and minor update task3 and task2

--- code_p2/task2.cpp
+++ code_p2/task2.cpp
@@ -2,9 +2,12 @@
 
 int main()
 {
+    /*
     int N = 6;
     double n = N + 1;
     double h = 1/n;
+
+    std::cout << "N = " << N << ", n = " << n << ", h = " << h << std::endl;
 
     double a = -1/std::pow(h,2);
     double d = 2/std::pow(h,2);
@@ -21,10 +24,17 @@ int main()
             A(i-1,i) = a; // Upper diagonal
         }
     }
+    */
 
-    A.print("Matrix A:");
-    
-    std::cout << "N = " << N << ", n = " << n << ", h = " << h << std::endl;
+    int N = 99;
+    double n = N + 1;
+    double h = 1/n;
+    double a = -1/std::pow(h,2);
+    double d = 2/std::pow(h,2);
+
+    arma::mat A = create_tridiagonal(N, a, d);
+
+    //A.print("Matrix A:");
 
     // Calculate eigenvals and eigenvec--------------------------------------------------
     arma::vec eigval;
@@ -33,16 +43,22 @@ int main()
     arma::eig_sym(eigval, eigvec, A); 
     // Return eigenvectors of unit norm 
 
-    eigval.print("Eigenvalues:");
-    eigvec.print("Eigenvectors:");
+    //eigval.print("Eigenvalues:");
+    //eigvec.print("Eigenvectors:");
 
     // Compare with analytical eigenvals and eigenvec -----------------------------------
     arma::vec a_eigval;
     arma::mat a_eigvec;
 
     std::tie(a_eigval, a_eigvec) = analytical(N, d, a);
-    a_eigval.print("Analytical Eigvalues");
-    a_eigvec.print("Analytical Eigenvectors");
+    //a_eigval.print("Analytical Eigvalues");
+    //a_eigvec.print("Analytical Eigenvectors");
+
+    
+    // Write data to output file
+    std::string filename = "data/analytical_eigvec_100.txt";
+    w_file(filename, a_eigvec);
+
     return 0;
 }
 
