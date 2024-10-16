e61288403f5bd2133a647f7766db0d6d189cf665
Mia Merlid <miame@uio.no>
2024-10-16 15:18:57 +0200

    Added loops for f and omega_V for time dep potential

--- code_p3/src/simulate_penning.cpp
+++ code_p3/src/simulate_penning.cpp
@@ -3,14 +3,15 @@
 #include "Particle.hpp"
 #include "constants.hpp"
 #include <filesystem>
+#include <armadillo>
 
-void create_directories() {
-    std::vector<std::string> dirs = {"data", "plots"};
+//void create_directories() {
+    //std::vector<std::string> dirs = {"data", "plots"};
     
-    for (const std::string& dir : dirs) {
-        std::filesystem::create_directories(dir);
-    }
-}
+    //for (const std::string& dir : dirs) {
+        //std::filesystem::create_directories(dir);
+    //}
+//}
 
 void simulate(std::vector<Particle> particles, PenningTrap trap, double dt, int timesteps, 
         std::string filename, bool interactions = true, std::string method = "RK4", bool time_dependent_E = false) {
@@ -32,7 +33,6 @@ void simulate(std::vector<Particle> particles, PenningTrap trap, double dt, int 
         for (int i = 0; i < timesteps; i++) {
             if (time_dependent_E) {
                 trap.update_time_dependent_V0(i*dt);
-                std::cout << trap.V0 << std::endl;
             }
         trap.save_to_file(filename, i*dt);
         trap.evolve_RK4(dt);
@@ -91,27 +91,31 @@ void simulate_traps_time_dependent_E(std::vector<Particle> particles, PenningTra
     double dt = 0.01;
     int timesteps = time/dt;
 
-    simulate(particles, trap, dt, timesteps, "data/one_particle_int_RK4_time_dep.txt", true, "RK4", true);
+    int size_particles = particles.size();
+    std::ostringstream filename;
+    filename << "data/no_int_" << "f_" << trap.f << "_w_v_" << trap.w_v << "_.txt";
+
+    simulate(particles, trap, dt, timesteps, filename.str(), false, "RK4", true);
 }
 
 
 int main() {
-    create_directories();
+    //create_directories();
 
     PenningTrap trap(B0_converted, V0_converted, d_const);
     std::vector<Particle> particles;
     particles.push_back(Particle(40.078, 1.0, {20.0, 0.0, 20.0}, {0.0, 25.0, 0.0})); // adding 1. proton
+    particles.push_back(Particle(40.078, 1.0, {25.0, 25.0, 0.0}, {0.0, 40.0, 5.0})); // adding 2. proton
 
-    // task 8
-    // simulate_traps_constant_E(particles, trap);
-
-
-
-    // task 9
-    // Implement a check that sets the external E- and B-fields to zero in the region outside the trap. 
-    // We’ll use the characteristic distance as a simple measure for the trap size, 
-    // so what we need is a check that sets the external fields E and B to zero when |r| > d.
-    trap.set_time_dependent_params(0.1, 0.2);
-    simulate_traps_time_dependent_E(particles, trap);
-
+    for (int i = 0; i < 8; i++) {
+        particles.push_back(Particle(40.078, 1.0, arma::vec(3).randn() * 0.1 * d_const, arma::vec(3).randn()*0.1*d_const)); // adding more protons
     }
+
+    for (double f = 0.1; f <= 0.7; f += 0.3) {
+        for (double w_v = 2.18; w_v <= 2.32; w_v += 0.02) {
+            trap.set_time_dependent_params(f, w_v);
+            simulate_traps_time_dependent_E(particles, trap);
+        }
+    }
+}
+    
